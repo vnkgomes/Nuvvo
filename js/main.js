edf3a8b7a69af72bdbb6e30efe46652799279697
Vinicius <249995415+vnkgomes@users.noreply.github.com>
2026-06-09 16:33:59 -0400

    js fix.

--- js/main.js
+++ js/main.js
@@ -80,12 +80,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Remove /index.html if present
             if (cleanPath.endsWith('/sobre.html')) {
-                cleanPath = cleanPath.slice(0, -10); // Remove 'index.html'
+                cleanPath = cleanPath.slice(0, -5); // Remove 'index.html'
             }
 
             // Remove /index.html if present
             if (cleanPath.endsWith('/portfolio.html')) {
-                cleanPath = cleanPath.slice(0, -14); // Remove 'index.html'
+                cleanPath = cleanPath.slice(0, -5); // Remove 'index.html'
             }
             
             // Remove trailing slash if present (optional)
